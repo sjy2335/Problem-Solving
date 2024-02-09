0be96199b9557dbfd0ffbf67369db204576886d9
sjy2335 <joey110951@gmail.com>
2024-02-09 14:05:46 +0900

    bj 1759

---
+++ backjun/1759_making_password.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int L, C; // L은 암호의 자릿수, C는 입력 알파벳 개수
+char A[15], P[15]; // A에 입력 알파벳 담고, P에 정답
+bool isVowel[26];
+
+void backtrack(int , int , int , int );
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    // 모음 bool 배열 초기화
+    isVowel[0] = isVowel[4] = isVowel[8] = isVowel[14] = isVowel[20] = true;
+    
+    cin >> L >> C; 
+    for(int i=0; i<C; i++){
+        cin >> A[i];
+    }
+
+    sort(A, A+C); // 먼저 입력 알파벳 정렬
+    
+    // 가능성 있는 암호(조건을 만족하는 알파벳 조합)를 모두 출력해야 함 -> 백트래킹!
+    backtrack(0, -1, 0, 0); // crnt, prev, consonant, vowel
+
+    return 0;
+}
+
+void backtrack(int crnt, int prev, int consonant, int vowel){
+    if (crnt == L){
+        if (consonant >= 2 && vowel >= 1) {
+            for (const auto &value: P){
+                cout << value; 
+            }
+            cout << '\n';
+        }
+        return;
+    } 
+    for (int i = prev+1; i<C; i++){
+        P[crnt] = A[i];
+        backtrack(crnt+1, i ,consonant+!isVowel[A[i]-'a'], vowel+isVowel[A[i]-'a']);
+    }
+}
