2c4d26b9163cbabef6c52bbf64bc351c8bff528b
sjy2335 <joey110951@gmail.com>
2024-03-25 18:46:05 +0900

    PS streak day 4 ing

---
+++ backjun/17298_NGE.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N, n;
+    cin >> N;
+    vector<int> A;
+
+    for (size_t i = 0; i < N; i++)
+    {
+        cin >> n;
+        A.push_back(n);
+    }
+
+    for (size_t i = 0; i < N; i++)
+    {
+        
+    }
+    
+    
+
+    return 0;
+}
