0409ccc1ea50e75aaf2b0dda2b8263210002bf6e
Sangjin Yoon <joey1109@g.skku.edu>
2024-06-10 01:05:06 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,14 @@
+function solution(n,a,b)
+{
+    var answer = 0;
+    
+
+    while (a != b) {
+        answer++;
+        a = Math.ceil(a/2);
+        b = Math.ceil(b/2);
+    }
+
+    return answer;
+}
+
