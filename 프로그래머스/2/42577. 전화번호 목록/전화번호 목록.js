d17619daed6c6c8eaa43fe5fe5279c490262eef3
Sangjin Yoon <joey1109@g.skku.edu>
2024-06-14 17:04:07 +0900

    [level 2] Title: 전화번호 목록, Time: 190.24 ms, Memory: 88.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,18 @@
+function solution(phone_book) {
+    
+    // 방법 1. 전체 phone_book 배열 정렬(문자열 기준) 후 이후 값이 이전 값으로 시작하는지 확인
+    
+    // 방법 2. 해시 맵 사용 => 우선 이거로 풀어보자.
+    const hashMap = new Map();
+    for (const number of phone_book) {
+        hashMap.set(number, true);
+    }
+    
+    for (const number of phone_book) {
+        for (let i = 0; i < number.length; i++) {
+            if (hashMap.get(number.slice(0, i)) === true) return false;
+        }
+    }
+    
+    return true;
+}
