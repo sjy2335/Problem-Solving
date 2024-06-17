5a40d1921488898cf0efa0f3fcb7a40a400f50ba
Sangjin Yoon <joey1109@g.skku.edu>
2024-06-18 07:18:15 +0900

    [goormlevel] Title: 어려운 문제, Time: 0.04 ms, Memory: 32.65 MB, Difficulty: 2 -BaekjoonHub

--- goormlevel/49054/1. 어려운 문제/어려운 문제.js
+++ goormlevel/49054/1. 어려운 문제/어려운 문제.js
@@ -4,32 +4,32 @@ const readline = require('readline');
 (async () => {
 	let rl = readline.createInterface({ input: process.stdin });
 	
+	const input = [];
 	for await (const line of rl) {
-		const N = Number(line);
-		if (N < 6) {
-			let A = factorial(N);
-			printing(A);
-		} else {
-			console.log('9');
-		}
+		input.push(line);
 		rl.close();
 	}
+	solution(input[0])
 	process.exit();
 })();
 
-function factorial(N) {
-	let dp = new Array(N+1).fill(0);
-	dp[0] = 1;
-	for (let i = 1; i <= N; i++) {
-		dp[i] = i * dp[i-1];
-		if (!(dp[i]%10)) dp[i] /= 10;
-	}
-	return dp[N];
-}
+function solution(N) {
+	let number = BigInt(1);
 
-function printing(A) {
-	while(parseInt(A/10)){
-		A = A.toString().split('').reduce((acc, crnt)=>acc+Number(crnt), 0);
+	for(let i = 1; i <= N; i++) {
+		number *= BigInt(i);
 	}
-	console.log(A);
+
+	while(number > 10) {
+		let sum = BigInt(0);
+		const temp = number.toString();
+
+		for(let i = 0; i < temp.length; i++) {
+			sum += BigInt(temp[i]);
+		}
+
+		number = sum;
+	}
+
+	console.log(number.toString());
 }
