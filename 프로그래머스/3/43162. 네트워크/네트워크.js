9bc4a01b5c583ce5140dc7f42d24ee0d6304d6c3
Sangjin Yoon <joey1109@g.skku.edu>
2024-06-21 12:48:21 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -4,6 +4,8 @@ function solution(n, computers) {
     
     const dfs = (i) => {
         if (!visited[i]) visited[i] = true;
+        // 왜 j를 i+1에서 시작하면 안되지? 무방향 그래프가 아닌가?
+        // 아 다른 질의 응답 보니 방향성이 있네.
         for (let j = 0; j < n; j++) {
             if (j !== i && computers[i][j] && !visited[j]) dfs(j);
         }
