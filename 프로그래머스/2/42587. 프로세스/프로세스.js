d9123737685c97180cab5193e851695a9ffe3d1d
Sangjin Yoon <joey1109@g.skku.edu>
2024-07-05 17:45:51 +0900

    [level 2] Title: 프로세스, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -0,0 +1,16 @@
+function solution(priorities, location) {
+    
+    priorities = priorities.map((priority, index) => [index, priority]);
+    
+    let cnt = 0;
+    while (priorities.length) {
+        if (priorities.findIndex(item => item[1] > priorities[0][1]) !== -1) {
+            // 더 높은 우선순위 있으면 뒤로 보냄
+            priorities.push(priorities.shift());
+        } else { // 가장 높은 우선순위면 바로 실행
+            let tmp = priorities.shift();
+            cnt++;
+            if (tmp[0] === location) return cnt;
+        }
+    }
+}
