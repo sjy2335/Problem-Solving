ad7cea76358bcfef22b1491c8ceded715edc8b21
Sangjin Yoon <joey1109@g.skku.edu>
2025-01-28 21:44:35 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,30 +1,26 @@
 class Solution {
     
-    private static boolean[] visited;
-	private static int[][] adj;
-    
-    private static void dfs(int crnt) {
-        visited[crnt] = true;
-        for (int i = 0; i < adj[crnt].length; i++) {
-            if (adj[crnt][i] == 1 && !visited[i]) {
-                dfs(i);
-            }
-        }
-    }
-    
     public int solution(int n, int[][] computers) {
-        
         int answer = 0;
-        adj = computers;
-        visited = new boolean[n];
-        
+        boolean[] visited = new boolean[n];
+        int[][] adj = computers;
+
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
                 answer++;
-                dfs(i);
+                dfs(i, visited, adj);
             }
         }
-        
+
         return answer;
+    }
+
+    private void dfs(int crnt, boolean[] visited, int[][] adj) {
+        visited[crnt] = true;
+        for (int i = 0; i < adj[crnt].length; i++) {
+            if (adj[crnt][i] == 1 && !visited[i]) {
+                dfs(i, visited, adj);
+            }
+        }
     }
 }
