7ae0481776f51885d9a674bb8eca354c3675e23e
Sangjin Yoon <joey1109@g.skku.edu>
2025-01-28 18:53:00 +0900

    [level 2] Title: 튜플, Time: 56.61 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,4 +1,21 @@
 import java.util.*;
+class Solution {
+    public int[] solution(String s) {
+        Set<String> set = new HashSet<>();
+        String[] arr = s.replaceAll("[{]", " ").replaceAll("[}]", " ").trim().split(" , ");
+        Arrays.sort(arr, (a, b)->{return a.length() - b.length();});
+        int[] answer = new int[arr.length];
+        int idx = 0;
+        for(String s1 : arr) {
+            for(String s2 : s1.split(",")) {
+                if(set.add(s2)) answer[idx++] = Integer.parseInt(s2);
+            }
+        }
+        return answer;
+    }
+}
+
+/*import java.util.*;
 
 class Solution {
     public int[] solution(String s) {
@@ -24,4 +41,4 @@ class Solution {
         
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
-}
+}*/
