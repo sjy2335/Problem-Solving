4ed7439c9f0b1e66b0b8caf515b747dced32f86e
Sangjin Yoon <joey1109@g.skku.edu>
2025-01-30 19:46:21 +0900

    [Silver III] Title: 주유소, Time: 308 ms, Memory: 31036 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    int N;
+    int[] roads;
+    int[] prices;
+
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+
+    public void solution() throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        roads = new int[N - 1];
+        prices = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N - 1; i++) {
+            roads[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            prices[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int totalPrice = 0;
+        for (int i = 0; i < N - 1;) {
+            // 다음 주유소가 현재보다 싸다면 최소한으로.
+            if (prices[i] > prices[i + 1]) {
+                totalPrice += prices[i] * roads[i];
+                i++;
+            // 다음 주유소가 현재보다 비싸면 다음 거리까지 포함해서.
+            // -> 더 싼 곳이 나올때까지.
+            } else {
+                int j = i;
+                int dist = 0;
+                while (j < N - 1) {
+                    dist += roads[j];
+                    if (prices[i] > prices[++j]) break;
+                }
+                totalPrice += prices[i] * dist;
+                i = j;
+            }
+        }
+
+        System.out.println(totalPrice);
+    }
+}
