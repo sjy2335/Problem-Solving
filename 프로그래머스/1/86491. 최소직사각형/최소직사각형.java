38effa2dbe736bb00b9e688346a427dc0985b919
Sangjin Yoon <joey1109@g.skku.edu>
2025-02-07 21:31:02 +0900

    [level 1] Title: 최소직사각형, Time: 6.05 ms, Memory: 87.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] sizes) {
+
+        int maxLength = 0;
+        for (int[] size: sizes) 
+            maxLength = Math.max(maxLength, Math.max(size[0], size[1]));
+
+		ArrayList<Integer> list = new ArrayList<>();
+        for (int[] size: sizes) {
+            list.add(Math.min(size[0], size[1]));
+        }
+        
+        return maxLength * list.stream().mapToInt(Integer::intValue).max().getAsInt();
+    }
+}
