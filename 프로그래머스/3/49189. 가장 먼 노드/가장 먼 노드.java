d566ce349a2fdd9e835349ceee6f4bdc0e08ef57
Sangjin Yoon <joey1109@g.skku.edu>
2025-02-14 14:45:20 +0900

    [level 3] Title: 가장 먼 노드, Time: 69.66 ms, Memory: 99.4 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,47 +1,35 @@
 import java.util.*;
 
 class Solution {
-    
-    private class Node {
-        int index;
-        int distance;
-        
-        Node(int index, int distance) {
-            this.index = index;
-            this.distance = distance;
-        }
-    }
-    
     public int solution(int n, int[][] edge) {
-       	
         List<Integer>[] adj = new ArrayList[n];
         for (int i = 0; i < n; i++) {
             adj[i] = new ArrayList<>();
-        }        
-        
-        for (int[] e: edge) {
+        }
+
+        for (int[] e : edge) {
             adj[e[0] - 1].add(e[1] - 1);
             adj[e[1] - 1].add(e[0] - 1);
         }
-        
+
         int[] dist = new int[n];
-        
-        Queue<Node> pq = new PriorityQueue<>((n1, n2) -> Integer.compare(n1.distance, n2.distance));
-        pq.add(new Node(0, 0));
-        
-        while (!pq.isEmpty()) {
-            Node crnt = pq.poll();
-            
-            for (int node: adj[crnt.index]) {
-                if (node != 0 && dist[node] == 0) {
-    	            pq.add(new Node(node, crnt.distance + 1));
-	                dist[node] = crnt.distance + 1;
+        Arrays.fill(dist, -1);
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(0);
+        dist[0] = 0;
+
+        while (!queue.isEmpty()) {
+            int crnt = queue.poll();
+
+            for (int next : adj[crnt]) {
+                if (dist[next] == -1) {
+                    dist[next] = dist[crnt] + 1;
+                    queue.add(next);
                 }
             }
         }
-        
+
         int max = Arrays.stream(dist).max().getAsInt();
-        
         return (int) Arrays.stream(dist).filter(i -> i == max).count();
     }
 }
