bc8c69b43fca93735e5198f32ff2dde429082cfa
Sangjin Yoon <joey1109@g.skku.edu>
2025-02-15 14:43:25 +0900

    [level 2] Title: 완전범죄, Time: 3.12 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.java
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -1,36 +1,6 @@
-// 13:51 ~ 
-import java.util.*;
-
 class Solution {
-    
-    private int[][] traces;
-    private int aLimit;
-    private int bLimit;
-
-    private Integer[][][] cache;
-    
-    
     public int solution(int[][] info, int n, int m) {
-        
-        traces = info;
-        aLimit = n;
-        bLimit = m;
-        
-        cache = new Integer[traces.length][n][m];
-        
-        int result = dp(0, 0, 0);
-        return result == Integer.MAX_VALUE ? -1 : result;
-    }
-    
-    private int dp(int idx, int aTrace, int bTrace) {
-        
-        if (aTrace >= aLimit || bTrace >= bLimit) return Integer.MAX_VALUE;
-        if (idx == traces.length) return aTrace;
-        if (cache[idx][aTrace][bTrace] != null) return cache[idx][aTrace][bTrace];
-        
-        int aStole = dp(idx + 1, aTrace + traces[idx][0], bTrace);
-        int bStole = dp(idx + 1, aTrace, bTrace + traces[idx][1]);
-        
-        return cache[idx][aTrace][bTrace] = Math.min(aStole, bStole);
+        int answer = 0;
+        return answer;
     }
 }
