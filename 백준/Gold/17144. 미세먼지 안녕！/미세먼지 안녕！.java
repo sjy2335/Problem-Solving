dc3c093559fc010e4f33d096279180311d861e5d
Sangjin Yoon <joey1109@g.skku.edu>
2025-03-06 14:57:48 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 328 ms, Memory: 40820 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -82,19 +82,51 @@ public class Main {
     }
 
     private void clean() {
-        // 위쪽 공기청정기 (반시계 방향)
-        for (int r = pr1 - 1; r > 0; r--) map[r][0] = map[r - 1][0];
-        for (int c = 0; c < C - 1; c++) map[0][c] = map[0][c + 1];
-        for (int r = 0; r < pr1; r++) map[r][C - 1] = map[r + 1][C - 1];
-        for (int c = C - 1; c > 1; c--) map[pr1][c] = map[pr1][c - 1];
-        map[pr1][1] = 0; // 공기청정기에서 처음 나온 공기
+        int[][] clone = new int[R][];
+        for (int i = 0; i < R; i++)
+            clone[i] = Arrays.copyOf(map[i], C);
 
-        // 아래쪽 공기청정기 (시계 방향)
-        for (int r = pr2 + 1; r < R - 1; r++) map[r][0] = map[r + 1][0];
-        for (int c = 0; c < C - 1; c++) map[R - 1][c] = map[R - 1][c + 1];
-        for (int r = R - 1; r > pr2; r--) map[r][C - 1] = map[r - 1][C - 1];
-        for (int c = C - 1; c > 1; c--) map[pr2][c] = map[pr2][c - 1];
-        map[pr2][1] = 0; // 공기청정기에서 처음 나온 공기
+        // 반시계 방향
+        dr = new int[]{0, -1, 0, 1};
+        dc = new int[]{1, 0, -1, 0};
+
+        int r = pr1;
+        int c = 1;
+        clone[r][c] = 0; // 청정기에서 처음 나온 공기
+
+        for (int i = 0; i < 4; i++) {
+            while (true) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                if (nr < 0 || nc < 0 || nr >= R || nc >= C) break;
+                if (map[nr][nc] == -1) break;
+                clone[nr][nc] = map[r][c];
+                r = nr;
+                c = nc;
+            }
+        }
+
+        // 시계 방향
+        dr = new int[]{0, 1, 0, -1};
+        dc = new int[]{1, 0, -1, 0};
+
+        r = pr2;
+        c = 1;
+        clone[r][c] = 0; // 청정기에서 처음 나온 공기
+
+        for (int i = 0; i < 4; i++) {
+            while (true) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                if (nr < 0 || nc < 0 || nr >= R || nc >= C) break;
+                if (map[nr][nc] == -1) break;
+                clone[nr][nc] = map[r][c];
+                r = nr;
+                c = nc;
+            }
+        }
+
+        map = clone;
     }
 
     private int count() {
