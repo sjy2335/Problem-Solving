85ed0e77bf81a0068551e91ba143950186e95170
Sangjin Yoon <joey1109@g.skku.edu>
2025-03-10 01:06:29 +0900

    [Silver III] Title: 어린 왕자, Time: 100 ms, Memory: 110736 KB -BaekjoonHub

--- 백준/Silver/1004. 어린 왕자/어린 왕자.py
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.py
@@ -1,12 +1,14 @@
-T = int(input())
+import sys
+
+T = int(sys.stdin.readline())
 while T > 0:
     T -= 1
-    x1, y1, x2, y2 = map(int, input().split())
-    n = int(input())
+    x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
+    n = int(sys.stdin.readline())
     count = 0
     while n > 0:
         n -= 1
-        cx, cy, cr = map(int, input().split())
+        cx, cy, cr = map(int, sys.stdin.readline().split())
         distPow1 = (x1 - cx) ** 2 + (y1 - cy) ** 2
         distPow2 = (x2 - cx) ** 2 + (y2 - cy) ** 2
         crPow = cr ** 2
